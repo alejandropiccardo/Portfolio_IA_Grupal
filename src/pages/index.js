682e86d0e33ba845dc6199578d7b5f63812c0d96
LUCAS MARTINO <LUCAS.MARTINO@correo.ucu.edu.uy>
2024-11-25 23:20:07 -0300

    update about

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,11 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Portafolio Alejandro Piccardo Machine Learning" />
+      <Seo title="Portafolio Alejandro Piccardo, Emiliano Labarthe, Lucas Martino y Juan Cabrera Machine Learning" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
-        <AboutSection sectionId="about" heading="Sobre mi" />
+        <AboutSection sectionId="about" heading="Sobre Nosotros" />
         <InterestsSection sectionId="details" heading="Herramientas:" />
         {/* <ProjectsSection sectionId="features" heading="Proyectos..." /> */}
         {/* <ContactSection sectionId="github" heading="Issues?" /> */}
